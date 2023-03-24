a18ac31ea1911163f9af7a4f9542654c52fb6935
aenon0 <95585844+aenon0@users.noreply.github.com>
2023-03-24 12:26:51 -0700

    Create set_mismatch.py

---
+++ set_mismatch.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        counting_arr = [0] * len(nums)
+        for num in nums:
+            counting_arr[num - 1] += 1
+        
+        result = [0, 0]
+        for indx in range(len(counting_arr)):
+            if counting_arr[indx] == 2:
+                result[0] = indx + 1
+            if counting_arr[indx] == 0:
+                result[1] = indx + 1
+        
+        return result
+        
